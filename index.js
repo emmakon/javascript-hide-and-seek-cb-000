a5623d6a68b08ffe6ff4bee8565f97bb0bb05a96
Emma Konrad <emma.konrad92@gmail.com>
2018-06-20 16:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const lis = document.querySelectorAll('ul.ranked-list li')
   
+  parseInt(n)
+  
   for (let i= 0; i < lis.length; i++) {
     lis[i].innerHTML = lis[i] + n;
   }
