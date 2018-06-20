694ca03fab18b0858c2ef2d779c93d4e94a157a4
Emma Konrad <emma.konrad92@gmail.com>
2018-06-20 16:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('ul.ranked-list li')
   
   for (let i= 0; i < lis.length; i++) {
-    x = parseInt(n);
+    const x = parseInt(n);
     lis[i].innerHTML = lis[i] + x;
   }
 }
